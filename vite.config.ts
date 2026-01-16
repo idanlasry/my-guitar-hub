51fed76c6d7684972aa660ee20c8d01fc8676f74
Idan Lasry <74493785+idanlasry@users.noreply.github.com>
2026-01-16 18:08:07 +0200

    feat: Update build and entry point for Vite
    Refactors the Vite configuration and entry point to align with common practices for React applications. This includes updating the `vite.config.ts` to specify the output directory and input HTML file, and renaming the main entry point from `index.tsx` to `main.tsx` in `index.html`. The `package.json` also sees minor dependency updates.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
   server: {
     host: true,
-  }
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
 });
